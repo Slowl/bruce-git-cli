f7a797ba353fe5ef04934ede3c0ad8187591a9ec
HediK <20215160+Slowl@users.noreply.github.com>
2022-02-11 00:08:07 +0100

    Help action and documentation (#8)
    * doc: added 'help' action to print documentation and updated readme
    
    Co-authored-by: hedi <hedi.kochad@bruce.work>

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# gBruce - A git CLI for Bruce.work
+# 📦 gBruce - A git CLI for Bruce.work
 
 A simple CLI to facilitate and automate some git actions, created and optimized for Bruce's conventions.
 
@@ -103,3 +103,9 @@ This action will make a push action with some automation. It will:
 | :--------------: | :-----: |:-------:|
 | `-b` `<name-of-the-branch-to-push>`| To use only if you want to push an other branch than -> | `<current-branch>` |
 | `-f`| Specify if you want to push --force | false |
+
+---
+
+#### `gbruce help`
+
+This action will show you all the available actions and their accepted argument(s)
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -20,7 +20,23 @@ export const information = {
 	git_pull_success: (baseBranch: string) => console.log(chalk.greenBright(`✓ Pulled latest changes of ${baseBranch}`)),
 	git_rebase_success: ({ currentBranch, baseBranch }: { currentBranch: string; baseBranch: string}) => 
 		console.log(chalk.greenBright(`✓ ${currentBranch} rebased with the latest changes of ${baseBranch}`)),
-	invalid_arguments: () => console.log(`${chalk.redBright('✗')} No valid arguments, see the list here https://github.com/Slowl/bruce-git-cli#utilisation`),
-	reminder_commit_push: () =>console.log(chalk.cyan('Don\'t forget to push your commit !'))
+	invalid_arguments: () => console.log(`${chalk.redBright('✗')} No valid action. Use the action ${chalk.greenBright('help')} to see available actions`),
+	reminder_commit_push: () => console.log(chalk.cyan('Don\'t forget to push your commit !')),
+	help: () => console.log(`
+• gbruce -- ${chalk.greenBright('branch')}: Create a new branch with Bruce's naming convention.
+
+• gbruce -- ${chalk.greenBright('commit')} ${chalk.redBright('[option]')}: Create a new commit with Bruce's naming convention.
+		${chalk.redBright('-r')}: Pull and rebase the base branch.
+		${chalk.redBright('-p')}: Push your commit.
+
+• gbruce -- ${chalk.greenBright('rebase')} ${chalk.redBright('[option]')}: Will pull and rebase to v3-development, or another defined branch.
+		${chalk.redBright('-b <name_of_a_branch')}: Define your base branch.
+
+• gbruce -- ${chalk.greenBright('push')} ${chalk.redBright('[option]')}: Will checkout and push the commit, and the branch if it doesn't exist in remote.
+		${chalk.redBright('-b <name_of_a_branch')}: Define the branch you want to push.
+		${chalk.redBright('-f')}: Push --force your commit.
+
+Check the README for more detailed informations: ${chalk.cyan('https://github.com/Slowl/bruce-git-cli')}
+`)
 }
 //#endregion
